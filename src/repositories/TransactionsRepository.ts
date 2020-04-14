04c02b279a6827328154ca4b6e80140e393afa26
palharez <xeduardopalhares@gmail.com>
2020-04-14 07:15:24 -0300

    feat: add routes and services transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private transactionsReducerByType(type: 'income' | 'outcome'): number {
+    return this.transactions.reduce((accumulator, transaction) => {
+      if (transaction.type === type) {
+        return transaction.value + accumulator;
+      }
+
+      return accumulator;
+    }, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactionsReducerByType('income');
+    const outcome = this.transactionsReducerByType('outcome');
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ type, title, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
